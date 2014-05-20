a9c193207e3d6b29dade73d13d08f0cd1ee5f75e
Chandler Kent <chandler.kent@gmail.com>
2014-05-20 22:48:35 +0000

    Remove debug output.

--- server/routes/subscribe.js
+++ server/routes/subscribe.js
@@ -3,9 +3,7 @@ var DB = require("../db");
 
 exports.list = {
   handler: function(request, reply) {
-    console.log("HERE");
     DB.smembers("subscribers", function(err, members) {
-      console.log(members);
       reply({
         subscribers: members
       });
