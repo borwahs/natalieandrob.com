547f2f194cef25a6fa2ecc33b339a4045aba4e45
Chandler Kent <chandler.kent@gmail.com>
2014-05-23 03:34:09 +0000

    Allow redis connection options to be configurable.

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
 var Redis = require("redis");
+var Config = require("./config");
 
-var db = Redis.createClient();
+var db = Redis.createClient(Config.redis.port, Config.redis.host);
 
 module.exports = db;
