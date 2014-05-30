a19ad42d8f9769d6e7c39e52d3b8ca3f50b47a24
Chandler Kent <chandler.kent@gmail.com>
2014-05-30 20:41:10 +0000

    Harp already defaults to 9000, so let's just use that.

--- Jakefile.js
+++ Jakefile.js
@@ -1,7 +1,7 @@
 desc('setup a client server for harp');
 task('default', [], function (params) {
   console.log('setup a client server for harp');
-  var cmd = 'harp server ./client --port 8888'
+  var cmd = 'harp server ./client --port 9000'
   jake.exec(cmd, {printStdout: true}, function () {
     complete();
   });
