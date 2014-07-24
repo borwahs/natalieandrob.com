1d5d39dca1fa0f0048440a6159afe894d8e5e147
Rob Shaw <rob@borwahs.com>
2014-07-23 23:19:45 -0400

    Split out all javascript file into separate files.
    - Main javascript file will initiate calls.
    - Subscriber Form holds handling of requests.
    - IntroAnimation will show and animate the SVG heart.

--- client/javascripts/introAnimation.js
+++ client/javascripts/introAnimation.js
@@ -14,22 +14,22 @@ function initHeartAnimation()
 {
   var heroClassEls = $(".hero");
   var unitClassEls = $(".unit");
-  var outerHeartEl = $("#outer-heart");
   var innerHeartEl = $("#inner-heart");
   var redAndBlackHeartEl = $("#red-black-heart");
-  
+  var outerHeartEl = $("#outer-heart");
+
   // Nothing to do if the heart doesn't exist
   if (outerHeartEl.length <= 0) { return; }
- 
+
   // hide the elements when document is ready so
   // it does not need to be done on the css elements
   heroClassEls.hide();
   unitClassEls.hide();
-  
+
   // To keep it clean, kick off the chain with a deferred
   var deferred = new $.Deferred();
   deferred.resolve();
-  
+
   deferred
   .then(wait(600))
   .then(function() { outerHeartEl.show(); })
@@ -39,7 +39,8 @@ function initHeartAnimation()
   .then(wait(2000))
   .then(function() { redAndBlackHeartEl.fadeOut(1400); })
   .then(function() { heroClassEls.fadeIn(1000); })
-  .then(function() { unitClassEls.fadeIn(1000); });
+  .then(function() { unitClassEls.fadeIn(1000); })
+  .then(function() { setPlayedAnimationCookie(true); });
 }
 
 function animateSVG(path)
@@ -53,35 +54,3 @@ function animateSVG(path)
   path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out';
   path.style.strokeDashoffset = '0';
 }
-
-function initSubscribeForm()
-{
-  var subscribeFormEl = $("#subscribe-form");
-  var subscribeFormSuccessEl = $("#subscribe-form-success");
-  var subscribeFormFailureEl = $("#subscribe-form-failure");
-  
-  subscribeFormEl.submit(function(evt) {
-    // Must make sure the failure is hidden if it is on the page
-    subscribeFormFailureEl.fadeOut("fast").addClass("hidden");
-    
-    var data = $(this).serialize();
-    var xhr = $.post("/subscribers", data);
-    xhr.done(function(response) {
-      console.log("Subscribed!", response);
-      subscribeFormEl.fadeOut("slow", function() {
-        $(this).hide();
-        subscribeFormSuccessEl.fadeIn("slow");
-      });
-    });
-    xhr.fail(function(response) {
-      subscribeFormFailureEl.fadeIn("slow");
-    });
-    
-    evt.preventDefault();
-  });
-}
-
-$(function() {
-  initHeartAnimation();
-  initSubscribeForm();
-});
