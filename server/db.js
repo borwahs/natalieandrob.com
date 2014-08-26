5894eade4ebf9e7c110c5b445e651f5251a9fe62
Rob Shaw <rob@borwahs.com>
2014-08-26 00:35:46 -0400

    Remove ] from connection string.

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ var pg = require("pg");
 var Config = require("./config");
 var Util = require("util");
 
-var conString = Util.format("postgres://%s:%s@%s:%s/%s]", Config.postgres.username, Config.postgres.password, Config.postgres.host, Config.postgres.port, Config.postgres.database);
+var conString = Util.format("postgres://%s:%s@%s:%s/%s", Config.postgres.username, Config.postgres.password, Config.postgres.host, Config.postgres.port, Config.postgres.database);
 
 var db = { connectionString: conString };
 
