15517f69c5a37231e01734914c4ac678a6868bd5
Rob Shaw <rob@borwahs.com>
2014-09-05 01:37:32 +0000

    Hook up Ember to the Reservation route.
    - Create Reservation object in Ember.
    - Update RsvpRoute to return reservation from server.
    - Add text if the RSVP Code is invalid on RSVP view.

--- client/rsvp/app/controllers/rsvp_controller.js
+++ client/rsvp/app/controllers/rsvp_controller.js
@@ -1,6 +1,25 @@
+RSVP.Reservation = Ember.Object.extend({
+  id: null,
+  rsvpCode: null
+});
+
 RSVP.RsvpController = Ember.Controller.extend({
   rsvpCode: null,
 
   actions: {
   }
+});
+
+RSVP.Reservation.reopenClass({
+  getReservation: function(rsvpCode) {
+    return $.getJSON("/reservation/" + rsvpCode).then(function(response) {
+
+      if (response.error)
+      {
+        return response;
+      }
+
+      return response.reservation;
+    });
+  },
 });
--- client/rsvp/app/routes/rsvp_route.js
+++ client/rsvp/app/routes/rsvp_route.js
@@ -1,11 +1,5 @@
 RSVP.RsvpRoute = Ember.Route.extend({
   model: function(params) {
-    return { rsvpCode: params.rsvp_code };
-  },
-  setupController: function(controller, model) {
-      var rsvpCode = model.rsvpCode ? model.rsvpCode : null;
-      if (rsvpCode) {
-          controller.set('rsvpCode', rsvpCode);
-      }
+    return RSVP.Reservation.getReservation(params.rsvp_code);
   }
 });
