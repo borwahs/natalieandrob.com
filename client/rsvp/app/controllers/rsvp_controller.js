855f08579dca09a0d1645d36ab4050e1b2b624bc
Rob Shaw <rob@borwahs.com>
2014-09-07 04:14:31 +0000

    Hook contact checkboxes up to reservation flags.

--- client/rsvp/app/controllers/rsvp_controller.js
+++ client/rsvp/app/controllers/rsvp_controller.js
@@ -1,15 +1,33 @@
 RSVP.RsvpController = Ember.Controller.extend({
   rsvpCode: null,
 
+  shouldDisableContactBigDayCheckboxes: function() {
+    return !this.get('model.isAttendingBigDay');
+  }.property('model.isAttendingBigDay'),
+
+  shouldDisableContactRehearsalDinnerCheckboxes: function() {
+    return !this.get('model.isAttendingRehearsalDinner');
+  }.property('model.isAttendingRehearsalDinner'),
+
   numWeddingAttendees: function() {
+    if (!this.get('model.isAttendingBigDay'))
+    {
+      return 0;
+    }
+
     var contacts = this.get('model.contacts');
     return contacts.filterBy('isAttendingBigDay', true).get('length');
-  }.property('model.contacts.@each.isAttendingBigDay'),
+  }.property('model.contacts.@each.isAttendingBigDay', 'model.isAttendingBigDay'),
 
   numRehearsalDinnerAttendees: function() {
+    if (!this.get('model.isAttendingRehearsalDinner'))
+    {
+      return 0;
+    }
+
     var contacts = this.get('model.contacts');
     return contacts.filterBy('isAttendingRehearsalDinner', true).get('length');
-  }.property('model.contacts.@each.isAttendingRehearsalDinner'),
+  }.property('model.contacts.@each.isAttendingRehearsalDinner', 'model.isAttendingRehearsalDinner'),
 
   actions: {
   }
