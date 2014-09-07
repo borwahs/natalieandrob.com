5bd0cdc4653de53ef69604d55fcfbad089bed23e
Rob Shaw <rob@borwahs.com>
2014-09-07 03:20:10 +0000

    Calculate number of attendees based on selected.
    - Add computed properties on the controller.
    - Fix issue with the reservation dinner value.
    - Show computed properties on the RSVP template.

--- client/rsvp/app/controllers/rsvp_controller.js
+++ client/rsvp/app/controllers/rsvp_controller.js
@@ -1,6 +1,16 @@
 RSVP.RsvpController = Ember.Controller.extend({
   rsvpCode: null,
 
+  numWeddingAttendees: function() {
+    var contacts = this.get('model.contacts');
+    return contacts.filterBy('isAttendingBigDay', true).get('length');
+  }.property('model.contacts.@each.isAttendingBigDay'),
+
+  numRehearsalDinnerAttendees: function() {
+    var contacts = this.get('model.contacts');
+    return contacts.filterBy('isAttendingRehearsalDinner', true).get('length');
+  }.property('model.contacts.@each.isAttendingRehearsalDinner'),
+
   actions: {
   }
 });
