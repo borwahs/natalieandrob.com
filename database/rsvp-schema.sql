84ce28c9465e2781c51608b65f320e23001a698a
Rob Shaw <rob@borwahs.com>
2014-09-26 21:57:57 +0000

    Add Contact table to rsvp-schema.

--- database/rsvp-schema.sql
+++ database/rsvp-schema.sql
@@ -1,11 +1,10 @@
-
 CREATE TABLE IF NOT EXISTS reservation (
   id                              SERIAL,
   reservationTitle                VARCHAR(255) NOT NULL,
   addressLineOne                  VARCHAR(500),
   addressLineTwo                  VARCHAR(500),
   addressCity                     VARCHAR(255),
-  addressState                    VARCHAR(255),
+  addressState                    VARCHAR(20),
   addressZipCode                  VARCHAR(20),
   rsvpCode                        VARCHAR(6) NOT NULL,
   emailAddress                    VARCHAR(255),
@@ -17,4 +16,14 @@ CREATE TABLE IF NOT EXISTS reservation (
   isAttendingRehearsalDinner      BOOLEAN,
   create_date                     TIMESTAMP NOT NULL,
   modified_date                   TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS contact (
+  id                          SERIAL,
+  firstName                   VARCHAR(255) NOT NULL,
+  middleName                  VARCHAR(255),
+  lastName                    VARCHAR(255) NOT NULL,
+  isChild                     BOOLEAN,
+  isAttendingBigDay           BOOLEAN,
+  isAttendingRehearsalDinner  BOOLEAN
 );
