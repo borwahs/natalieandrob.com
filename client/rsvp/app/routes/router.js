0ea633b76f6e8e80cc53657603261f86054a43c5
Chandler Kent <chandler.kent@gmail.com>
2014-09-29 23:50:15 +0000

    Beginnings of the multi-step RSVP process.
    For now, the controllers and the routes are defined in a single file.
    
    There are functionalities for moving between the routes.
    
    We need a way to have a common state so we aren't going back to the server each time. For now this is a global.

--- client/rsvp/app/routes/router.js
+++ client/rsvp/app/routes/router.js
@@ -3,4 +3,8 @@ require('client/rsvp/app/routes/rsvp_route.js');
 
 RSVP.Router.map(function() {
   this.route('rsvp', { path: '/:rsvp_code' });
+  this.route('rsvp-attendance', { path: '/:rsvp_code/attendance' });
+  this.route('rsvp-attendees', { path: '/:rsvp_code/attendees' });
+  this.route('rsvp-notes', { path: '/:rsvp_code/notes' });
+  this.route('rsvp-wrap-up', { path: '/:rsvp_code/wrap-up' });
 });
--- client/rsvp/app/routes/rsvp_route.js
+++ client/rsvp/app/routes/rsvp_route.js
@@ -1,5 +1,29 @@
+RSVP.CurrentRsvp = null;
+
 RSVP.RsvpRoute = Ember.Route.extend({
   model: function(params) {
     return RSVP.Reservation.getReservation(params.rsvp_code);
+  },
+  
+  afterModel: function(model, transition, queryParams) {
+    // according to the docs (http://emberjs.com/api/classes/Ember.Route.html#method_afterModel)
+    // queryParams should be non-empty
+    // console.logging proves otherwise
+    
+    // we should enhance this to transition to their last known state
+    RSVP.CurrentRsvp = model;
+    this.transitionTo("rsvp-attendance", model.get("rsvpCode"));
   }
 });
+
+RSVP.RsvpBaseRoute = Ember.Route.extend({
+  model: function() {
+    // TODO: This is kinda hacky
+    return RSVP.CurrentRsvp;
+  }
+});
+
+RSVP.RsvpAttendanceRoute = RSVP.RsvpBaseRoute.extend();
+RSVP.RsvpAttendeesRoute = RSVP.RsvpBaseRoute.extend();
+RSVP.RsvpNotesRoute = RSVP.RsvpBaseRoute.extend();
+RSVP.RsvpWrapUpRoute = RSVP.RsvpBaseRoute.extend();
