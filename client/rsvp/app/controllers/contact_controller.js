b4c850eabc6d63efae4c4ba766ec85b70f01b793
Rob Shaw <rob@borwahs.com>
2014-10-01 04:25:07 +0000

    Disable the First/Last Name textfields if not attending.

--- client/rsvp/app/controllers/contact_controller.js
+++ client/rsvp/app/controllers/contact_controller.js
@@ -3,6 +3,14 @@ RSVP.ContactController = Ember.ObjectController.extend({
     return this.get('isAttendingBigDay') == 1 ? "Yes" : "No";
   }.property('isAttendingBigDay'),
 
+  disableControlsIfNotAttendingBigDay: function() {
+    return this.get('isAttendingBigDay') != 1;
+  }.property('isAttendingBigDay'),
+
+  isAttendingRehearsalDinnerBoolean: function() {
+  return this.get('isAttendingRehearsalDinner') == 1;
+  }.property('isAttendingRehearsalDinner'),
+
   actions: {
     setIsAttendingValue: function() {
       this.set('isAttendingBigDay', this.get('isAttendingBigDay') == 1 ? 0 : 1);
