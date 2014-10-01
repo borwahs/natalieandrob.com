861a2d322af039f80eaf2de78fda7ce3d8f70b32
Rob Shaw <rob@borwahs.com>
2014-10-01 05:20:24 +0000

    Finish design of the Attendees view (#12).
    - Restructure the view for better responsive design.
    - Add properties to contact controller.
    - Update styling to make input controls nicer looking.

--- client/rsvp/app/controllers/contact_controller.js
+++ client/rsvp/app/controllers/contact_controller.js
@@ -1,19 +1,49 @@
 RSVP.ContactController = Ember.ObjectController.extend({
+  attendingButtonCSSClass: function() {
+    return this.get('isAttendingBigDay') == 1 ? "attend-button selected" : "attend-button";
+  }.property('isAttendingBigDay'),
+
+  attendingRehearsalDinnerButtonCSSClass: function() {
+    return this.get('isAttendingRehearsalDinner') == 1 ? "attend-button selected" : "attend-button";
+  }.property('isAttendingRehearsalDinner'),
+
+  guestName: function() {
+    var firstName = this.get('firstName');
+    var lastName = this.get('lastName');
+
+    var firstNameLength = this.get('firstName.length');
+    var lastNameLength = this.get('lastName.length');
+
+    if (firstNameLength == 0 || lastNameLength == 0) {
+      firstName = "Guest";
+      lastName = "Name";
+    }
+
+    return firstName + ' ' + lastName;
+  }.property('firstName', 'lastName'),
+
   isAttendingButtonText: function() {
     return this.get('isAttendingBigDay') == 1 ? "Yes" : "No";
   }.property('isAttendingBigDay'),
+
+  isAttendingRehearsalDinnerButtonText: function() {
+    return this.get('isAttendingRehearsalDinner') == 1 ? "Yes" : "No";
+  }.property('isAttendingRehearsalDinner'),
 
   disableControlsIfNotAttendingBigDay: function() {
     return this.get('isAttendingBigDay') != 1;
   }.property('isAttendingBigDay'),
 
-  isAttendingRehearsalDinnerBoolean: function() {
+  disableControlsIfNotAttendingRehearsalDinner: function() {
   return this.get('isAttendingRehearsalDinner') == 1;
   }.property('isAttendingRehearsalDinner'),
 
   actions: {
     setIsAttendingValue: function() {
       this.set('isAttendingBigDay', this.get('isAttendingBigDay') == 1 ? 0 : 1);
+    },
+    setIsAttendingRehearsalDinnerValue: function() {
+      this.set('isAttendingRehearsalDinner', this.get('isAttendingRehearsalDinner') == 1 ? 0 : 1);
     }
   }
 });
